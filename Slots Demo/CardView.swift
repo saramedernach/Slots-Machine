ea6e69926c4178570ad40bbfbcc99d73e07cd776
Sara Medernach <saramedernach@ucsb.edu>
2020-05-02 18:23:15 -0700

    Slots machine game

---
+++ Slots Demo/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  Slots Demo
+//
+//  Created by Sara Medernach on 4/30/20.
+//  Copyright © 2020 Sara Medernach. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @Binding var symbol:String
+    @Binding var background:Color
+    
+    var body: some View {
+        
+        Image(symbol)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .background(background.opacity(0.5))
+            .cornerRadius(20)
+        
+    }
+    
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(symbol: Binding.constant("cherry"), background: Binding.constant(Color.green))
+    }
+}
